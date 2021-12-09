e1a21d597a59f7307b04d47a824d61f7fccfa8ae
hardiktalwar <hardik.talwar@knoldus.com>
2021-12-09 21:05:02 +0530

    OOPs Assignment Submission

---
+++ src/main/scala/oops_2/Person.scala
@@ -0,0 +1,24 @@
+package oops_2
+
+class Person(var name:String,var age:Int) extends Ordered[Person] {
+   override def compare(that: Person): Int = {
+    this.name compare that.name match {
+      case 0 =>  if(this.age.equals(that.age))
+                    println("True\nName & age both are same")
+                  else
+                    println("False\nName are same but ages are not equal")
+                  0
+
+      case _ =>  if(this.name.length.equals(that.name.length) && this.age.equals(that.age))
+                        println("True")
+                      else println("false")
+                      1
+
+    }
+  }
+}
+object Person extends  App{
+  var personOne = new Person("Hardik",22)
+  var personTwo = new Person("Shivam",28)
+  personOne compare personTwo
+}
